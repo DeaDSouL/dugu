3de31f73bfea63071b1627d20895e2b73af067c6
DeaDSouL <DeaDSouL@users.noreply.github.com>
2020-11-23 17:53:20 +0300

    docs(dugu.app_input): update usage help docs

--- src/dugu/app_input.py
+++ src/dugu/app_input.py
@@ -47,12 +47,14 @@ def parse_args() -> argparse.Namespace:
                         help='Increase output verbosity. (not fully implemented yet)')
     # --------------------------------------------------------------------------------------------------------------
     parser.add_argument('-s', '--symlinks', action='store_true', default=False,
-                        help='Include symbolic links in the scanning. (not recommended)')
+                        help='Follow symbolic links that point to files, even if they are located out of the '
+                             'target-directory. (not recommended)')
     parser.add_argument('-S', '--follow-symlinks', dest='follow_symlinks', action='store_true', default=False,
-                        help='Follow symbolic links in the scanning. (not recommended)')
+                        help='Follow symbolic links that point to directories. even if they are located out of the '
+                             'target-directory. (not recommended)')
     # --------------------------------------------------------------------------------------------------------------
     parser.add_argument('-f', '--force', action='store_true', default=False,
-                        help='Ignoring the generated cache.')
+                        help='Ignoring the generated cache. And re-generate a new one.')
     # --------------------------------------------------------------------------------------------------------------
     parser.add_argument('-t', '--hashtype', type=str, default='md5',
                         choices=['md5', 'sha1', 'sha256', 'sha512'],
@@ -61,78 +63,58 @@ def parse_args() -> argparse.Namespace:
     group = parser.add_mutually_exclusive_group()
     # --------------------------------------------------------------------------------------------------------------
     group.add_argument('-p', '--print_duplicates', action='store_true', default=False,
-                       help='''will print all the duplicated files categorized by
-                       their sets. Please note that, this argument is meant
-                       to only work with argument "scan"; So if it is being
-                       called with argument "precopy", it will first execute
-                       the "precopy" to "dir1", then will execute the same
-                       selected optional arguments with "scan" to the
-                       produced directory "%s".''' % DUGU_UNIQUE_FILES_DIR)
+                       help='''will print all the duplicated files categorized by their sets. Please note that, this 
+                       argument is meant to only work with argument "scan"; So if it is being called with argument 
+                       "precopy", it will first execute the "precopy" to "dir1", then will execute the same selected 
+                       optional arguments with "scan" to the produced directory "%s".''' % DUGU_UNIQUE_FILES_DIR)
     # --------------------------------------------------------------------------------------------------------------
     group.add_argument('-l', '--soft-links', dest='soft_links', action='store_true', default=False,
-                       help='''will generate a folder that has soft-links to all the
-                       duplicated files categorized by their sets. Please
-                       note that, this argument is meant to only work with
-                       argument "scan"; So if it is being called with
-                       argument "precopy", it will first execute the
-                       "precopy" to "dir1", then will execute the same
-                       selected optional arguments with "scan" to the
-                       produced directory "%s".''' % DUGU_UNIQUE_FILES_DIR)
+                       help='''will generate a folder that has soft-links to all of the duplicated files categorized by 
+                       their sets. Please note that, this argument is meant to only work with argument "scan"; So if it 
+                       is being called with argument "precopy", it will first execute the "precopy" to "dir1", then will 
+                       execute the same selected optional arguments with "scan" to the produced directory "%s".'''
+                            % DUGU_UNIQUE_FILES_DIR)
     group.add_argument('-L', '--hard-links', dest='hard_links', action='store_true', default=False,
-                       help='''will generate a folder that has hard-links to all the
-                       duplicated files categorized by their sets. Please
-                       note that, this argument is meant to only work with
-                       argument "scan"; So if it is being called with
-                       argument "precopy", it will first execute the
-                       "precopy" to "dir1", then will execute the same
-                       selected optional arguments with "scan" to the
-                       produced directory "%s".''' % DUGU_UNIQUE_FILES_DIR)
+                       help='''will generate a folder that has hard-links to all of the duplicated files categorized by 
+                       their sets. Please note that, this argument is meant to only work with argument "scan"; So if it 
+                       is being called with argument "precopy", it will first execute the "precopy" to "dir1", then will 
+                       execute the same selected optional arguments with "scan" to the produced directory "%s".'''
+                            % DUGU_UNIQUE_FILES_DIR)
     # --------------------------------------------------------------------------------------------------------------
     group.add_argument('-i', '--isolate', action='store_true', default=False,
-                       help='''will move the duplicated files to a folder called
-                       "isolate" under the temporary folder in your
-                       operating-system. Please note that, this argument is
-                       meant to only work with argument "scan"; So if it is
-                       being called with argument "precopy", it will first
-                       execute the "precopy" to "dir1", then will execute the
-                       same selected optional arguments with "scan" to the
-                       produced directory "%s".''' % DUGU_UNIQUE_FILES_DIR)
+                       help='''will move the duplicated files to a folder called "isolate" under the temporary folder in 
+                       your operating-system. Please note that, this argument is meant to only work with argument 
+                       "scan"; So if it is being called with argument "precopy", it will first execute the "precopy" to 
+                       "dir1", then will execute the same selected optional arguments with "scan" to the produced 
+                       directory "%s".''' % DUGU_UNIQUE_FILES_DIR)
     # --------------------------------------------------------------------------------------------------------------
     group.add_argument('-r', '--remove', action='store_true', default=False,
-                       help='''will prompt user for files to preserve and remove all
-                       others; important: under particular circumstances,
-                       data may be lost when using this option together with
-                       -s or --symlinks. Please note that, this argument is
-                       meant to only work with argument "scan"; So if it is
-                       being called with argument "precopy", it will first
-                       execute the "precopy" to "dir1", then will execute the
-                       same selected optional arguments with "scan" to the
-                       produced directory "%s".''' % DUGU_UNIQUE_FILES_DIR)
+                       help='''will prompt user for files to preserve and remove all others; important: under particular 
+                       circumstances, data may be lost when using this option together with -s or --symlinks. Please 
+                       note that, this argument is meant to only work with argument "scan"; So if it is being called 
+                       with argument "precopy", it will first execute the "precopy" to "dir1", then will execute the 
+                       same selected optional arguments with "scan" to the produced directory "%s".'''
+                            % DUGU_UNIQUE_FILES_DIR)
     group.add_argument('-R', '--autoremove', action='store_true', default=False,
-                       help='''will preserve the first file in each set of duplicates
-                       and remove the rest without prompting the user. Please
-                       note that, this argument is meant to only work with
-                       argument "scan"; So if it is being called with
-                       argument "precopy", it will first execute the
-                       "precopy" to "dir1", then will execute the same
-                       selected optional arguments with "scan" to the
-                       produced directory "%s".''' % DUGU_UNIQUE_FILES_DIR)
+                       help='''will preserve the first file in each set of duplicates and remove the rest without 
+                       prompting the user. Please note that, this argument is meant to only work with argument "scan"; 
+                       So if it is being called with argument "precopy", it will first execute the "precopy" to "dir1", 
+                       then will execute the same selected optional arguments with "scan" to the produced directory 
+                       "%s".''' % DUGU_UNIQUE_FILES_DIR)
     # --------------------------------------------------------------------------------------------------------------
     subparsers = parser.add_subparsers(help='Main Commands', dest='cmd')
     # --------------------------------------------------------------------------------------------------------------
     parser_scan = subparsers.add_parser('scan',
-                                        help='''(ex: scan "path/to/dir") To preform a normal scan for
-                                        the duplicated files, followed by the
-                                        path of the directory.''')
+                                        help='''(ex: scan "path/to/dir") To preform a normal scan for the duplicated 
+                                        files, followed by the path of the directory.''')
     parser_scan.add_argument('DIR', type=str, nargs='?', action='store',
                              default=os_getcwd(), help='The path of the directory to scan.')
     # --------------------------------------------------------------------------------------------------------------
     parser_precopy = subparsers.add_parser('precopy',
-                                           help='''(ex: precopy "path/to/dir1" "path/to/dir2"): To
-                                           exclude all the files in "dir1" that already in
-                                           "dir2", and put the unique files in a folder called
-                                           "%s" inside "dir1" with the same structure
-                                           of "dir1".''' % DUGU_UNIQUE_FILES_DIR)
+                                           help='''(ex: precopy "path/to/dir1" "path/to/dir2"): To exclude all the files 
+                                           in "dir1" that already in "dir2", and put the unique files in a folder called
+                                           "%s" inside "dir1" with the same structure of "dir1".'''
+                                                % DUGU_UNIQUE_FILES_DIR)
     parser_precopy.add_argument('DIRS', type=str, nargs=2, action='store',
                                 help='Expects 2 dirs: DIR_FROM and DIR_TO.')
     # --------------------------------------------------------------------------------------------------------------
